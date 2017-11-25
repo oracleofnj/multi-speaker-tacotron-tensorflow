a2bf000d0689a2e3ab8cde32211b35864ac146a2
Jared Samet <jared.samet@aya.yale.edu>
2017-11-25 17:33:57 +0000

    Fix segment extractor

--- extract-tedlium/create-segment-extractor.py
+++ extract-tedlium/create-segment-extractor.py
@@ -6,7 +6,7 @@ def create_script(segments_filename, sph_path, output_path):
   with open(segments_filename) as f:
     for line in f:
       segment_id, file_id, start_time, end_time = line.strip().split(' ')
-      print('sph2pipe -f wav -t {0}:{1} -p {2} > {3}.wav'.format(
+      print('sph2pipe -f wav -t {0}:{1} -p {2}.sph > {3}.wav'.format(
             start_time, end_time,
             os.path.join(sph_path, file_id),
             os.path.join(output_path, segment_id)
