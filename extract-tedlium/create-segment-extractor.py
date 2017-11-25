6b8083248725d580fe046f3e8df45c9045cc4f7c
Jared Samet <jared.samet@aya.yale.edu>
2017-11-25 17:32:01 +0000

    Add segment extractor

---
+++ extract-tedlium/create-segment-extractor.py
@@ -0,0 +1,17 @@
+"""Create a set of commands to extract the segments into individual .WAVs."""
+import os
+import sys
+
+def create_script(segments_filename, sph_path, output_path):
+  with open(segments_filename) as f:
+    for line in f:
+      segment_id, file_id, start_time, end_time = line.strip().split(' ')
+      print('sph2pipe -f wav -t {0}:{1} -p {2} > {3}.wav'.format(
+            start_time, end_time,
+            os.path.join(sph_path, file_id),
+            os.path.join(output_path, segment_id)
+      ))
+
+if __name__ == "__main__":
+  create_script(sys.argv[1], sys.argv[2], sys.argv[3])
+
